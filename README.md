cbffde1435acb0e56e2713b99a416839b611fc44
Gray Morrow <tgrahammor@gmail.com>
2020-08-28 16:31:59 -0600

    input and error messages; calculate change functionality not added yet

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Homework, Week 1: Make Change
+
 ## Make Change Project
 
 ### Overview
@@ -7,10 +9,11 @@
 ### Topics
 * Taking User Input
 * Casting
+* Methods
 
 ### How to Run
 
 1. Compile the `MakeChange.java` class file with `javac` or load it in Eclipse or another IDE.
 2. Run the file from the command line with `java MakeChange`, or with your IDE's run tools.
 3. Enter the inputs requested by the program.
-4. ~~PROFIT!~~ Look at console output for the answers you seek. 
+4. ~~PROFIT!~~ Look at console output for the answers you seek.
--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -3,8 +3,38 @@ package makechange;
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		float price;
+		float tender;
+		boolean paidEnough = true;
+		
+		//User Story #1:	The user is prompted asking for the price of the item.
+		System.out.print("Please enter the price of them item: ");
+		price = kb.nextFloat();
+		
+		//User Story #2:	The user is then prompted asking how much money was tendered by the customer.
+		System.out.print("How much money did the customer tender? $");
+		tender = kb.nextFloat();
+		
+		//User Story #3:	Display an appropriate message if the customer provided too little money or the exact amount.
+		if ( tender < price ) {
+			System.err.println("Not enough money tendered. Cannot proceed with transaction");
+		} else if ( tender == price ) {
+			System.out.println("Exact change provided!");
+		} else {
+			calculateChange(price, tender);
+		}
+		
+		//User Story #4:	If the amount tendered is more than the cost of the item, display the number of bills and coins that should be given to the customer.
+	
+		kb.close();
+	}
+	
+	public static void calculateChange ( float due, float paid ) {
+	
+		
+		return;
 	}
 
 }
